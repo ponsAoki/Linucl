ffeed2e056bd767bc985ca0c11e7e3a52c4fff93
nakashi94 <n0904yBUNJOU@gmail.com>
2022-07-02 18:10:03 +0900

    front: ヘッダーとフッターを完成
    TopPageとHomePageとTypingGamePageにヘッダーとフッターを設定

--- front/src/pages/TopPage.tsx
+++ front/src/pages/TopPage.tsx
@@ -1,9 +1,12 @@
 import { FC, memo } from "react";
+import { Footer, Header } from "../components/organisms/layout";
 
 export const TopPage: FC = memo(() => {
   return (
     <div>
+      <Header />
       <div>TopPage</div>
+      <Footer />
     </div>
   )
 })
--- front/src/pages/TypingGamePage.tsx
+++ front/src/pages/TypingGamePage.tsx
@@ -1,9 +1,12 @@
 import { FC, memo } from "react";
+import { Footer, Header } from "../components/organisms/layout";
 
 export const TypingGamePage: FC = memo(() => {
   return (
     <div>
+      <Header />
       <div>TypingGamePage</div>
+      <Footer />
     </div>
   )
 })
