d976dac3973761b65bcea984749c72fcb21a3e43
nakashi94 <n0904yBUNJOU@gmail.com>
2022-07-03 04:07:11 +0900

    front: TypingGamePageの外観を作成

--- front/src/pages/TypingGamePage.tsx
+++ front/src/pages/TypingGamePage.tsx
@@ -5,7 +5,17 @@ export const TypingGamePage: FC = memo(() => {
   return (
     <div>
       <Header />
-      <div>TypingGamePage</div>
+      <div className="bg-gray-100 py-16">
+        <div className="box-border border-4 rounded-lg outline-none w-2/3 px-8 py-12 mx-auto my-16 text-center">
+          <p className="text-2xl mb-12">問題文</p>
+          <input
+            type="text"
+            placeholder="解答欄"
+            className="outline-none rounded-md text-center text-3xl my-12"
+          />
+          <div className="mt-12">タイムゲージ</div>
+        </div>
+      </div>
       <Footer />
     </div>
   )
