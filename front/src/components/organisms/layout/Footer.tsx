a9d5a0a9572043f8fdab7209bf56217f8858d88e
nakashi94 <n0904yBUNJOU@gmail.com>
2022-07-02 18:20:20 +0900

    front: ヘッダーとフッターを作成
    TopPageとHomePageとTypingGamePageにヘッダーとフッターを設定

--- front/src/components/organisms/layout/Footer.tsx
+++ front/src/components/organisms/layout/Footer.tsx
@@ -2,7 +2,7 @@ import { FC, memo } from "react";
 
 export const Footer: FC = memo(() => {
   return(
-    <div className="border-t-2 w-4/5 mx-auto text-center">
+    <div className="border-t-2 w-4/5 mx-auto text-center mt-12">
       <small>&copy; Linucl</small>
     </div>
   )
