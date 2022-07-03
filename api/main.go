ffff5952a8443c61c021c648b22c972e79ffb14a
ponsAoki <aokishogo20pk@gmail.com>
2022-07-03 22:58:03 +0900

    [add] #? DB接続したけど、データ取得できてない

--- api/main.go
+++ api/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"example.com/go-apis/configs"
+	"example.com/go-apis/controllers"
+)
+
+func main() {
+	router := controllers.Router()
+
+	//アプリケーション起動時にデータベース接続
+	configs.ConnectDB()
+
+	router.Run("localhost:8000")
+}
