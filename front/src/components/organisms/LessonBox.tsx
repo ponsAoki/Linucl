8999fe49f108edc2941db5035a93db710f4662db
nakashi94 <n0904yBUNJOU@gmail.com>
2022-07-05 03:07:25 +0900

    HomePageのデザインとボタンを変更

--- front/src/components/organisms/LessonBox.tsx
+++ front/src/components/organisms/LessonBox.tsx
@@ -10,18 +10,22 @@ export const LessonBox: FC<Props> = memo((props) => {
   const { outline, content } = props;
   let navigate: NavigateFunction = useNavigate();
   return (
-    <div className="box-border border-4 rounded-lg outline-none w-1/2 mx-auto my-16 text-center">
+    <div className="box-border border-4 rounded-lg outline-none w-1/2 mx-auto my-16 text-white text-center shadow-lg shadow-zinc-500">
       <h3 className="text-3xl font-bold my-6">{outline}</h3>
       <p className="px-8 my-6">{content}</p>
       <div className="flex justify-around my-6">
         <button
-          className="text-white font-bold w-48 rounded-lg bg-blue-400 px-4 py-2 hover:opacity-80"
+          className="text-white font-bold w-48 rounded-lg bg-yellow-400 hover:bg-yellow-500 px-4 py-2 shadow-yellow2 opacity-90 hover:opacity-100"
           onClick={() => navigate("/typing-game")}
-        >タイピングゲーム</button>
+        >
+          タイピングゲーム
+        </button>
         <button
-          className="text-white font-bold w-48 rounded-lg bg-blue-400 px-4 py-2 hover:opacity-80"
+          className="text-white font-bold w-48 rounded-lg bg-sky-400 hover:bg-sky-500 px-4 py-2 shadow-sky2 opacity-90 hover:opacity-100"
           onClick={() => navigate("/quiz")}
-        >クイズ</button>
+        >
+          クイズ
+        </button>
       </div>
     </div>
   )
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     extend: {
       boxShadow: {
-        '3xl': '0 5px 0px #4f96f6, 0 10px 100px #4f96f6',
+        'yellow1': '0 5px 0px #eab308, 0 10px 100px #fef08a',
+        'yellow2': '0 5px 0px #ca8a04,0 7px 30px #fde047',
+        'sky1': '0 5px 0px #0ea5e9,0 7px 30px #bae6fd',
+        'sky2': '0 5px 0px #0284c7,0 7px 30px #7dd3fc',
       }
     },
   },
