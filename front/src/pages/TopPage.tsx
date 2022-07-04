d1dcd31fbe19e96cb3f7eed3b0fa824dd3200a3f
nakashi94 <n0904yBUNJOU@gmail.com>
2022-07-05 03:16:50 +0900

    TopPageのボタンデザインを変更

--- front/src/pages/TopPage.tsx
+++ front/src/pages/TopPage.tsx
@@ -19,23 +19,17 @@ export const TopPage: FC = memo(() => {
           <div className="text-center pt-12">
             <h1 className="text-white md:text-8xl sm:text-7xl text-6xl px-4 font-black">L&thinsp;I&thinsp;N&thinsp;U&thinsp;C&thinsp;L</h1>
             <ul className="my-12">
-              <li>
+              <li className="my-8">
                 <button
-                  className="text-white font-bold w-48 rounded-lg bg-blue-400 px-4 py-2 mb-4 hover:opacity-80"
+                  className="text-white font-black text-lg w-48 rounded-lg bg-yellow-400 shadow-yellow1 px-4 py-2 mb-4 hover:bg-yellow-500 hover:shadow-yellow2"
                   onClick={() => navigate("#")}
                 >
                   ログイン
                 </button>
-                {/* <button
-                  className="relative flex justify-center items-center mx-auto my-8 bg-blue-500 text-white font-bold rounded duration-75 ease-in-out shadow-[0_5px_0_#4f96f6, 0_10px_100px_#4f96f6]" 
-                  onClick={() => navigate("#")}
-                >
-                  ログイン
-                </button> */}
               </li>
               <li>
                 <button
-                  className="text-white font-bold w-48 rounded-lg bg-blue-400 px-4 py-2 hover:opacity-80"
+                  className="text-white font-black text-lg w-48 rounded-lg bg-yellow-400 shadow-yellow1 px-4 py-2 mb-4 hover:bg-yellow-500 hover:shadow-yellow2"
                   onClick={() => navigate("/home")}
                 >
                   ゲストで始める
