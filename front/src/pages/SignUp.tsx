6a2f5cd74738f4d3a4ad34b8e574bd0999bc3544
nakashi94 <n0904yBUNJOU@gmail.com>
2022-10-21 16:14:37 +0900

    edit SignIn, SignUp pages

--- front/src/pages/SignUp.tsx
+++ front/src/pages/SignUp.tsx
@@ -1,28 +1,38 @@
 import { createRef, FC, memo, RefObject, useRef } from "react";
+import { Link } from "react-router-dom";
 import { useSendSignInData } from "../hooks/auth/useSendSignInData";
 
 export const SignUp: FC = memo(() => {
   const { sendSignInData } = useSendSignInData();
   const refs = useRef<RefObject<HTMLInputElement>[]>([]);
-  const FormName = ["email", "password"]; // refを複数定義するために必要
+  const FormName = ["userName", "email", "password", "rePassword"]; // refを複数定義するために必要
   FormName.forEach((_, i) => {
     refs.current[i] = createRef();
   })
 
   return (
-    <div className="h-screen justify-center flex items-center">
+    <div className="h-screen justify-center flex items-center bg-zinc-900 text-gray-200">
       <div className="box-border w-96 h-[32rem] px-16 py-12 border-4 rounded-xl">
-        <h3 className="text-base font-medium">Authentication App</h3>
-        <h2 className="text-xl mt-6 mb-8 font-bold">SignIn</h2>
+        <h3 className="text-base font-medium text-center">LINUCL</h3>
+        <h2 className="text-xl mt-6 mb-8 font-bold">Sign&nbsp;up</h2>
         <form>
+          <input
+            type="userName"
+            name="userName"
+            required
+            pattern="[ -~]{1,16}"
+            placeholder="ユーザーネーム"
+            className="text-gray-900 w-full px-4 py-2 mb-3 outline-none border-2 rounded-lg"
+            ref={refs.current[0]}
+          />
           <input
             type="email"
             name="email"
             required
             pattern="^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"
             placeholder="メールアドレス"
-            className="w-full px-4 py-2 mb-3 outline-none border-2 rounded-lg focus:border-sky-500"
-            ref={refs.current[0]}
+            className="text-gray-900 w-full px-4 py-2 mb-3 outline-none border-2 rounded-lg"
+            ref={refs.current[1]}
           />
           <input
             type="password"
@@ -30,19 +40,32 @@ export const SignUp: FC = memo(() => {
             required
             pattern="\w{8,}"
             placeholder="パスワード"
-            className="w-full px-4 py-2 mb-4 outline-none border-2 rounded-lg focus:border-sky-500"
-            ref={refs.current[1]}
+            className="text-gray-900 w-full px-4 py-2 mb-4 outline-none border-2 rounded-lg"
+            ref={refs.current[2]}
+          />
+          <input
+            type="rePassword"
+            name="rePassword"
+            required
+            pattern="\w{8,}"
+            placeholder="パスワード(確認用)"
+            className="text-gray-900 w-full px-4 py-2 mb-4 outline-none border-2 rounded-lg"
+            ref={refs.current[3]}
           />
           <button
             className="w-full rounded-lg py-2 bg-blue-500 text-white font-medium hover:opacity-80"
             onClick={() => sendSignInData(refs.current[0].current!.value, refs.current[1].current!.value)}
           >
-            Sign in
+            Sign&nbsp;up
           </button>
         </form>
-        <div className="mt-6 text-sm text-gray-600 text-center">別の方法でログインする</div>
-        <button className="mt-2 mb-4">sample</button>
-        <div className="text-sm text-gray-600 text-center">アカウントをお持ちでない方はこちら</div>
+        {/* <div className="mt-6 text-sm text-gray-600 text-center">別の方法で登録する</div> */}
+        <br />
+        {/* <button className="mt-2 mb-4">sample</button> */}
+        <div className="text-sm text-center">
+          アカウントをお持ちの方は
+          <Link to="/sign-in" className="text-blue-500">こちら</Link>
+        </div>
       </div>
     </div>
   )
